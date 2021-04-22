069d43de0580858a18742ba64288e671db9a5ec3
bsidd004 <80121338+bsidd004@users.noreply.github.com>
2021-04-22 16:55:42 -0400

    created admin backend files

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+let app = require("express")();
+let bodyParser = require("body-parser");
+let mongoose = require("mongoose");
+let cors = require("cors");
+
+let url="";
+
+const mongooseDbOption ={       // to avoid warning 
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+
+mongoose.connect(url,mongooseDbOption);
+mongoose.connection;
+var Admin=require("./router/admin.router.js");
