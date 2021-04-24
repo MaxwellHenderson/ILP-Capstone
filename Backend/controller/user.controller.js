bb769abb679309fd1502788e999d090bb5161f5f
Max <xthatonekid@gmail.com>
2021-04-23 20:10:56 -0700

    Update user.controller.js
    Added methods for my features to the user controller

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,33 +1,56 @@
-let UserModel=require("../model/user.model.js");
+let UserModel = require("../model/user.model.js");
 
-let getUser=(req,res)=>{
-    let userName=req.params.UserName;
-    let userPassword=req.params.UserPassword;
+let getUser = (req, res) => {
+  let userName = req.params.UserName;
+  let userPassword = req.params.UserPassword;
 
-    UserModel.find({"userName":userName,"userPassword":userPassword},(err,data)=>{
-        if(!err){
+  UserModel.find(
+    { userName: userName, userPassword: userPassword },
+    (err, data) => {
+      if (!err) {
+      } else {
+      }
+    }
+  );
+};
 
-        }else{
-
+let updateUserInfo = (req, res) => {
+  let userName = req.body.userName;
+  let updatedPassword = req.body.userPassword;
+  UserModel.updateOne(
+    { userName: userName },
+    { $set: { userPassword: updatedPassword } },
+    (err, result) => {
+      if (!err) {
+        if (result.nModified > 0) {
+          res.send("Record updated succesfully");
+        } else {
+          res.send("Record is not available");
         }
-    })
+      } else {
+        res.send("Error generated " + err);
+      }
+    }
+  );
+};
+
+//req.body.cart is an object of product objects
+//req.body.userId is the _id of the the user who owns the cart
+let createCart = (req, res) => {
+  UserModel.updateOne(
+    { _id: req.body.userId },
+    { $set: { cart: req.body.cart } },
+    (err, result) => {
+      if (!err) {
+      }
+    }
+  );
+};
+
+//Gets the user object from the other function
+//Then retrieves just the cart field and returns it
+let getCart = (req, res) => {
+    let user = getUser()
 }
 
-let updateUserInfo= (req,res)=> {
-    let userName = req.body.userName;
-    let updatedPassword = req.body.userPassword;
-    UserModel.updateOne({userName:userName},{$set:{userPassword:updatedPassword}},(err,result)=> {
-        if(!err){
-            if(result.nModified>0){
-                    res.send("Record updated succesfully")
-            }else {
-                    res.send("Record is not available");
-            }
-        }else {
-            res.send("Error generated "+err);
-        }
-    })
-
-}
-
-module.exports={getUser, updateUserInfo};
+module.exports = { getUser, updateUserInfo };
