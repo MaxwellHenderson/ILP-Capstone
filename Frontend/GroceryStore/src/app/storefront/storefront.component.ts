de91b70465b3273d180b161ea84760607be99cca
Max <xthatonekid@gmail.com>
2021-04-26 12:48:40 -0700

    Storefront displaying products

--- Frontend/GroceryStore/src/app/storefront/storefront.component.ts
+++ Frontend/GroceryStore/src/app/storefront/storefront.component.ts
@@ -9,13 +9,19 @@ import { Product } from '../shared/product.model';
 })
 export class StorefrontComponent implements OnInit {
   products?: Array<Product>;
+  dataLoaded: boolean = false;
 
   constructor(public productService: ProductService) {}
 
   ngOnInit(): void {
-    this.productService
-      .getAllProductDetails()
-      .subscribe((result) => (this.products = result));
-    console.log(this.products);
+    this.productService.getAllProductDetails().subscribe((result) => {
+      this.products = result;
+      this.dataLoaded = true;
+      console.log(this.products);
+    });
+  }
+
+  addProductToCart(productId: any) {
+    console.log(productId);
   }
 }
