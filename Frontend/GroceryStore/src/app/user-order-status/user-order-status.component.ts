7078a140d9dcd40fc1be8b72eeee27a6a310fb22
Max <maxwell.blair.henderson@gmail.com>
2021-04-27 09:51:26 -0700

    Updated order.model file to be in the shared folder, added code for creating order/checkout. Currently not adding the cart item correctly

--- Frontend/GroceryStore/src/app/user-order-status/user-order-status.component.ts
+++ Frontend/GroceryStore/src/app/user-order-status/user-order-status.component.ts
@@ -1,28 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-import { Order } from '../mode.Order';
+import { Order } from '../shared/order.model';
 import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-user-order-status',
   templateUrl: './user-order-status.component.html',
-  styleUrls: ['./user-order-status.component.css']
+  styleUrls: ['./user-order-status.component.css'],
 })
 export class UserOrderStatusComponent implements OnInit {
-  resultMsg?:string;
-  orderArr:Array<Order>=[]
-  constructor(public orderSer:OrderService) { }
+  resultMsg?: string;
+  orderArr: Array<Order> = [];
+  constructor(public orderSer: OrderService) {}
 
   ngOnInit(): void {
-    let uname = JSON.parse(sessionStorage.getItem("userInfo")!);
-    this.orderSer.retrieveOrderById(uname).subscribe(result=> {
-      console.log(result)
-      this.orderArr=result
-      
-      }
-    
-    );
-    
-
-    }
-   
+    let uname = JSON.parse(sessionStorage.getItem('userInfo')!);
+    this.orderSer.retrieveOrderById(uname).subscribe((result) => {
+      console.log(result);
+      this.orderArr = result;
+    });
+  }
 }
