c108de88bc0ec4c4cc193b4cef8b44ce33344a8b
MounishaK <mounisha098@gmail.com>
2021-04-29 12:01:16 -0500

    Generate reports - product, customers

--- Frontend/GroceryStore/src/app/shared/order.model.ts
+++ Frontend/GroceryStore/src/app/shared/order.model.ts
@@ -16,7 +16,7 @@ export class OrderReport {
 }
 
 export class User {
-  constructor(public _id:number,public userName:string,public userLastName:string,public userDobdob:Date,public userAddress:string,public userEmail:string,public userPassword:string){}
+  constructor(public _id:number,public userName:string,public userLastName:string,public userDob:Date,public userAddress:string,public userEmail:string,public userPassword:string){}
 }
 
 export class Product {
