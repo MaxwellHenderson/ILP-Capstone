30a9001e113540164cac7d99ff43b89ef2533a7b
Max <xthatonekid@gmail.com>
2021-04-29 17:15:49 -0700

    update server files

--- Frontend/GroceryStore/src/app/backend-url.service.ts
+++ Frontend/GroceryStore/src/app/backend-url.service.ts
@@ -8,7 +8,7 @@ export class BackendUrlService {
   // backendUrl: string = `http://localhost:9090`;
 
   //Use if running on AWS
-  backendUrl: string = `ec2-34-221-237-5.us-west-2.compute.amazonaws.com:9090`;
+  backendUrl: string = `http://34.221.237.5:9090`;
   constructor() {}
   getBackendUrl() {
     return this.backendUrl;
